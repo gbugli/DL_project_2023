3798322b1a25a784a015e6008031caf0bdf80ecd
Laurent Baeriswyl <laurentbaeriswyl@protonmail.com>
2023-04-23 18:43:09 -0400

    Add validation to pretrain script

--- pretrain_JEPA.py
+++ pretrain_JEPA.py
@@ -19,14 +19,13 @@ def parse_args() -> Namespace:
     parser = ArgumentParser("JEPA")
     # parser.add_argument("--config-file-name", required=True, type=str, help="Name of json file with config")
     parser.add_argument("--root", help="Name of dir with data", required=True, type=str)
-    parser.add_argument("--annotation-file", help="Name of dir with annotation files",
-                        required=True, type=str)
+    parser.add_argument("--val-dir", help="Name of dir with validation data", required=True, type=str)
     parser.add_argument("--output-dir", required=True, type=str, help="Name of dir to save the checkpoints to")
     parser.add_argument("--resume", default=False, type=bool, help="In case training was not completed resume from last epoch")
 
     return parser.parse_args()
 
-def load_data(root, annotation_file):
+def load_data(root, annotation_file, batch_size=2):
     preprocess = transforms.Compose([
             ImglistToTensor(),  # list of PIL images to (FRAMES x CHANNELS x HEIGHT x WIDTH) tensor
             # transforms.Resize(299),  # image batch, resize smaller edge to 299
@@ -47,7 +46,35 @@ def load_data(root, annotation_file):
 
     dataloader = torch.utils.data.DataLoader(
             dataset=dataset,
-            batch_size=2,
+            batch_size=batch_size,
+            shuffle=True,
+            num_workers=1,
+            pin_memory=True
+        )
+    return dataloader
+
+def load_validation_data(val_dir, val_annotation_file, batch_size=2):
+    preprocess = transforms.Compose([
+            ImglistToTensor(),  # list of PIL images to (FRAMES x CHANNELS x HEIGHT x WIDTH) tensor
+            # transforms.Resize(299),  # image batch, resize smaller edge to 299
+            transforms.Resize((128,128)),
+            # transforms.CenterCrop(299),  # image batch, center crop to square 299x299
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ])
+
+    dataset = VideoFrameDataset(
+        root_path=val_dir,
+        annotationfile_path=val_annotation_file,
+        num_segments=1,
+        frames_per_segment=22,
+        imagefile_template='image_{:d}.png',
+        transform=preprocess,
+        test_mode=False
+    )
+
+    dataloader = torch.utils.data.DataLoader(
+            dataset=dataset,
+            batch_size=batch_size,
             shuffle=True,
             num_workers=1,
             pin_memory=True
@@ -55,7 +82,7 @@ def load_data(root, annotation_file):
     return dataloader
 
 # Train the model
-def train_model(epoch, model, criterion, optimizer, scheduler, dataloader, num_epochs, output_dir, device):
+def train_model(epoch, model, criterion, optimizer, scheduler, dataloader, val_dataloader, num_epochs, output_dir, device):
     while epoch < num_epochs:
         model.train()
         train_loss = 0
@@ -77,8 +104,21 @@ def train_model(epoch, model, criterion, optimizer, scheduler, dataloader, num_e
         avg_epoch_loss = train_loss / len(dataloader)
         #scheduler.step()
 
+        ### Validation
+        with model.eval():
+          val_loss = 0
+          for i, data in enumerate(val_dataloader, 0):
+              inputs, labels = data
+              #inputs, labels = inputs.to(device), labels.to(device)
+
+              prediction_blocks, target_blocks = model(inputs.transpose(1, 2))
+              loss = criterion(prediction_blocks, target_blocks)
+              val_loss += loss.item()
+          
+          avg_epoch_val_loss = val_loss / len(val_dataloader)
+        
         current_lr = optimizer.param_groups[0]['lr']
-        print(f"Epoch: {epoch + 1}, Learning Rate: {current_lr:.6f}, Average epoch loss: {avg_epoch_loss:.4f}")
+        print(f"Epoch: {epoch + 1}, Learning Rate: {current_lr:.6f}, Average epoch loss: {avg_epoch_loss:.4f}, Average epoch val loss: {avg_epoch_val_loss:.4f}")
 
         # TO DO: Implement Early Stopping?? Based on what?
 
@@ -104,14 +144,22 @@ if __name__ == "__main__":
 
     device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
 
-    args = parse_args()
-    dataloader = load_data(args.root,args.annotation_file)
-
     # All these hyperparamters we might want to have a config file to choose them and use a custom Config class to parse
     num_epochs = 10
-    total_steps = num_epochs * len(dataloader)
     div_factor = 10 # max_lr/div_factor = initial lr
     final_div_factor = 100 # final lr is initial_lr/final_div_factor 
+    batch_size = 128
+
+    args = parse_args()
+
+    # Load unlabeled data and validation data
+    unlabeled_data_dir  = os.path.join(args.root, 'data')
+    unlabeled_annotation_dir = os.path.join(args.root, 'annotations.txt')
+    val_data_dir  = os.path.join(args.val_dir, 'data')
+    val_annotation_dir = os.path.join(args.val_dir, 'annotations.txt')
+
+    dataloader = load_data(unlabeled_data_dir, unlabeled_annotation_dir, batch_size)
+    val_dataloader = load_validation_data(val_data_dir, val_annotation_dir, batch_size)
 
     # Used this approach so that we can get back to training the loaded model from checkpoint
     epoch = 0
@@ -141,6 +189,9 @@ if __name__ == "__main__":
                         time_drop_rate=0.1)
     criterion = nn.MSELoss()
     optimizer = torch.optim.AdamW(model.parameters(), lr=0.001, weight_decay=0.0005)
+
+    # Define One Cycle LR Scheduler
+    total_steps = num_epochs * len(dataloader)
     scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr=0.003, total_steps=total_steps, div_factor=div_factor, final_div_factor=final_div_factor)
 
     if args.resume:
@@ -153,5 +204,5 @@ if __name__ == "__main__":
             scheduler.load_state_dict(checkpoint['scheduler_state_dict'])
             epoch = checkpoint['epoch']
 
-    results = train_model(epoch, model, criterion, optimizer, scheduler, dataloader, num_epochs, args.output_dir, device)
+    results = train_model(epoch, model, criterion, optimizer, scheduler, dataloader, val_dataloader, num_epochs, args.output_dir, device)
     print(f'Model training finshed at epoch {results["epoch"]}, trainig loss: {results["train_loss"]}')
