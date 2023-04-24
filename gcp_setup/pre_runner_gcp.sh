a0ac4efbee3acb402b1714c16d12257a454bde0a
Giacomo Bugli <gbugli@icloud.com>
2023-04-23 23:58:02 -0400

    gcp setup fixes

--- gcp_setup/pre_runner_gcp.sh
+++ gcp_setup/pre_runner_gcp.sh
@@ -1,11 +1,7 @@
-scp -r $USER@greene-dtn.hpc.nyu.edu:/scratch/gb2572/Dataset_Student.tar.gz /scratch/$USER/
-
-tar -xvzf Dataset_Student.tar.gz
+scp -r $USER@greene-dtn.hpc.nyu.edu:/scratch/gb2572/Dataset_Student.sqsh /scratch/$USER/
 
 mkdir /scratch/$USER/singularity_files/
 
 scp $USER@greene-dtn.hpc.nyu.edu:/scratch/wz2247/singularity/images/pytorch_22.08-py3.sif /scratch/$USER/singularity_files/
 
 scp $USER@greene-dtn.hpc.nyu.edu:/scratch/work/public/overlay-fs-ext3/overlay-15GB-500K.ext3.gz /scratch/$USER/singularity_files/
-
-# scp $USER@greene-dtn.hpc.nyu.edu:/scratch/work/public/overlay-fs-ext3/overlay-5GB-3.2M.ext3.gz /scratch/$USER/singularity_files/
