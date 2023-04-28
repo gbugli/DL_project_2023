Fixed rank computation

pretrain_JEPA.py
@@ -67,7 +67,7 @@ def mean_cosine_similarity(embeddings):
   return mean, sense_check
 
 
-def compute_rank(prediction_blocks, threshold=0.95):
+def compute_rank(prediction_blocks, threshold=0.85):
   embeddings = prediction_blocks.reshape(-1, prediction_blocks.size(-1))
 
   sample_mean = torch.mean(embeddings, dim=0, keepdim=True)
@@ -82,11 +82,10 @@ def compute_rank(prediction_blocks, threshold=0.95):
 
   return torch.sum(cumulative_explained_variance_ratio < threshold) + 1
 
-def compute_rank_per_frame(prediction_blocks, threshold=0.95):
+def compute_rank_per_frame(prediction_blocks, threshold=0.85):
   frame_ranks = torch.zeros(prediction_blocks.size(1), dtype=torch.int64)
   for i in range(prediction_blocks.size(1)):
     frame_ranks[i] = compute_rank(prediction_blocks[:, i, :, :], threshold)
-  
   return frame_ranks
 
 # Train the model
@@ -127,7 +126,7 @@ def train_model(epoch, model, criterion, optimizer, scheduler, dataloader, val_d
               target_cos_sim, sense_check = mean_cosine_similarity(target_blocks)
               pred_cos_sim, sense_check = mean_cosine_similarity(prediction_blocks)
               print(f"Current loss: {loss.item()}")
-              print(f"Overall pred rank: {compute_rank(prediction_blocks)}, Context rank per frame: {compute_rank_per_frame(context_embeddings)}")
+              print(f"Overall pred rank: {compute_rank(prediction_blocks)}, Context rank per frame: {compute_rank_per_frame(rearrange(context_embeddings, 'b (t n) m -> b t n m', t=22))}")
               print(f"Context cos_sim: {context_cos_sim}, Target cos_sim: {target_cos_sim}, Pred cos_sim: {pred_cos_sim}, Sense check: {sense_check}")
 
             # Update the learning rate using the scheduler