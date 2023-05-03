c17243d031b13afe1df93e34b47eb51b94ae6da6
Giacomo Bugli <gb2572@b-6-4.c.hpc-slurm-9c75.internal>
2023-05-02 21:57:30 -0400

    Last changes to code

--- LSTM/ConvLSTM_train.py
+++ LSTM/ConvLSTM_train.py
@@ -25,6 +25,7 @@ from utils.file_utils import create_output_dirs, PathsContainer
 from argparse import ArgumentParser, Namespace
 
 from dataset.video_dataset import VideoFrameDataset, ImglistToTensor, MaskDataset
+from dataset.dataset_utils import load_data
 
 from models.Seq2Seq import Seq2Seq
 from utils.early_stop import EarlyStop
@@ -56,35 +57,6 @@ def collate(batch):
                    
     return input, target 
 
-
-def load_data(root, annotation_file, batch_size=2, mask=False, shuffle=True):
-    preprocess = transforms.Compose([
-            ImglistToTensor(),  # list of PIL images to (FRAMES x CHANNELS x HEIGHT x WIDTH) tensor
-            # transforms.Resize(299),  # image batch, resize smaller edge to 299
-            # transforms.Resize((160,240)),
-            # transforms.CenterCrop(299),  # image batch, center crop to square 299x299
-            transforms.Normalize((0.61749697, 0.6050092, 0.52180636), (2.1824553, 2.1553133, 1.9115673)),
-        ])
-
-    dataset = VideoFrameDataset(
-        root_path=root,
-        annotationfile_path=annotation_file,
-        num_segments=1,
-        frames_per_segment=22,
-        imagefile_template='image_{:d}.png',
-        transform=preprocess,
-        mask=mask,
-        test_mode=False
-    )
-
-    dataloader = torch.utils.data.DataLoader(
-            dataset=dataset,
-            batch_size=batch_size,
-            shuffle=shuffle,
-            num_workers=2, # arbitrarily chosen
-            pin_memory=True
-        )
-    return dataloader
 
 def one_hot_encoding(input_tensor, num_classes=49):
     one_hot = F.one_hot(input_tensor.long(), num_classes)
--- LSTM/UNet_pretraining.py
+++ LSTM/UNet_pretraining.py
@@ -20,6 +20,7 @@ from argparse import ArgumentParser, Namespace
 from utils.file_utils import create_output_dirs, PathsContainer
 
 from dataset.video_dataset import VideoFrameDataset, ImglistToTensor
+from dataset.dataset_utils import load_data
 
 from models.UNet import UNet
 from utils.early_stop import EarlyStop
@@ -39,34 +40,6 @@ def parse_args() -> Namespace:
     return parser.parse_args()
 
 
-def load_data(root, annotation_file, batch_size=2, mask=False, shuffle=True):
-    preprocess = transforms.Compose([
-            ImglistToTensor(),  # list of PIL images to (FRAMES x CHANNELS x HEIGHT x WIDTH) tensor
-            # transforms.Resize(299),  # image batch, resize smaller edge to 299
-            # transforms.Resize((160,240)),
-            # transforms.CenterCrop(299),  # image batch, center crop to square 299x299
-            transforms.Normalize((0.61749697, 0.6050092, 0.52180636), (2.1824553, 2.1553133, 1.9115673)),
-        ])
-
-    dataset = VideoFrameDataset(
-        root_path=root,
-        annotationfile_path=annotation_file,
-        num_segments=1,
-        frames_per_segment=22,
-        imagefile_template='image_{:d}.png',
-        transform=preprocess,
-        mask=mask,
-        test_mode=False
-    )
-
-    dataloader = torch.utils.data.DataLoader(
-            dataset=dataset,
-            batch_size=batch_size,
-            shuffle=shuffle,
-            num_workers=2, # arbitrarily chosen
-            pin_memory=True
-        )
-    return dataloader
 
 def train_model(epoch, model, trainloader, valloader, optimizer, criterion, scheduler, early_stop, device, num_epochs, output_dir):
     while epoch < num_epochs:
